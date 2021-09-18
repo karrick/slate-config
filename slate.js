686354dc55e62a5ee49e11f37df4dfcacb06e32a
Karrick S. McDermott <karrick@karrick.dev>
2021-09-18 12:40:54 -0400

    remove unused and commented out config lines

--- slate.js
+++ slate.js
@@ -24,36 +24,6 @@ KS.register(KS.mode("height-half"), ["6:ctrl,cmd"]);
 KS.register(KS.mode("height-third"), ["7:ctrl,cmd"]);
 KS.register(KS.mode("height-two-thirds"), ["8:ctrl,cmd"]);
 
-// KS.register(KS.mode("full-screen"), ["1:ctrl,cmd"]);
-// KS.register(KS.mode("left-half"), ["2:ctrl,cmd"]);
-// KS.register(KS.mode("right-half"), ["3:ctrl,cmd"]);
-// KS.register(KS.mode("left-third"), ["4:ctrl,cmd"]);
-// KS.register(KS.mode("middle-third"), ["5:ctrl,cmd"]);
-// KS.register(KS.mode("right-third"), ["6:ctrl,cmd"]);
-// KS.register(KS.mode("right-two-thirds"), ["7:ctrl,cmd"]);
-// KS.register(KS.mode("top-half"), ["8:ctrl,cmd"]);
-// KS.register(KS.mode("bottom-half"), ["9:ctrl,cmd"]);
-
-// KS.register(KS.op("grow-left"), ["left:ctrl,cmd"]);
-// KS.register(KS.op("shrink-left"), ["left:shift,ctrl,cmd"]);
-
-// KS.register(KS.op("nudge", "left"), ["left:shift,ctrl,cmd"]);
-// KS.register(KS.op("nudge", "right"), ["right:shift,ctrl,cmd"]);
-// KS.register(KS.op("nudge", "up"), ["up:shift,ctrl,cmd"]);
-// KS.register(KS.op("nudge", "down"), ["down:shift,ctrl,cmd"]);
-
-// KS.register(KS.op("resize", "grow-horizontal"), ["right:shift,ctrl,cmd"]);
-// KS.register(KS.op("resize", "shrink-horizontal"), ["left:shift,ctrl,cmd"]);
-
-// KS.register(KS.op("resize", "grow-vertical"), ["up:shift,ctrl,cmd"]);
-// KS.register(KS.op("resize", "shrink-vertical"), ["down:shift,ctrl,cmd"]);
-
-// KS.register(KS.op("resize", "full-width"), ["right:ctrl,alt,cmd"]);
-// KS.register(KS.op("resize", "half-width"), ["left:ctrl,alt,cmd"]);
-
-// KS.register(KS.op("resize", "full-height"), ["up:ctrl,alt,cmd"]);
-// KS.register(KS.op("resize", "half-height"), ["down:ctrl,alt,cmd"]);
-
 KS.register(KS.op("resize", "grow"), ["g:ctrl,cmd"]);
 // KS.register(KS.op("resize", "shrink"), ["s:ctrl,cmd"]);
 
